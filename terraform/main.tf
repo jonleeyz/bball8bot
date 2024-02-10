d406322cdfdaf433fbdf3d17d493dd0153d0be04
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-10 10:57:35 +0800

    Provision SQS resource (#23)

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ module "lambda" {
   lambda_name           = "bball8bot_event_handler"
   handler_function_name = "main"
 }
+
+module "sqs" {
+  source     = "./sqs"
+  queue_name = "bball8bot_event_queue"
+  is_fifo    = false
+}
