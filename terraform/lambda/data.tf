71b87c8429d0f25ae871d237e0ce03d37b48284c
jonlee <lee.jonathan@hey.com>
2024-02-10 08:30:15 +0800

    Create archive file data resource block
    This data resource block will zip the golang executable binary so the Lambda OS-only runtime can execute it.

--- terraform/lambda/data.tf
+++ terraform/lambda/data.tf
@@ -21,3 +21,10 @@ data "aws_iam_policy_document" "assume_bball8bot_lambda_role" {
     }
   }
 }
+
+# Creates a zip file at the output path from the specified source file path.
+data "archive_file" "zipped_binary_for_deploy" {
+  type        = "zip"
+  source_file = local.binary_path
+  output_path = local.output_archive_path
+}
