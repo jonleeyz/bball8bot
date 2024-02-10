f959a72f84acda7e6f35595df4bcfa17bbd32f2e
jonlee <lee.jonathan@hey.com>
2024-02-10 08:33:20 +0800

    Create Lambda function resource block

---
+++ terraform/lambda/inputs.tf
@@ -0,0 +1,9 @@
+variable "lambda_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "handler_function_name" {
+  description = "The name of the golang handler function"
+  type        = string
+}
--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1,3 +1,14 @@
+resource "aws_lambda_function" "bball8bot_event_handler" {
+  function_name = var.lambda_name
+  handler       = var.handler_function_name
+
+  role = aws_iam_role.bball8bot_lambda_role.arn
+
+  # Ref: https://docs.aws.amazon.com/lambda/latest/dg/runtimes-provided.html
+  runtime  = "provided.al2023"
+  filename = local.output_archive_path
+}
+
 resource "aws_iam_policy" "bball8bot_lambda_policy" {
   name   = "bball8botLambdaPolicy"
   path   = "/"
