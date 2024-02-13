5e4cc24e610e832492839fa64d3b1826ffb357b6
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-13 10:23:37 +0800

    Update Terraform apply outputs (#32)
    * Add outputs for API Gateway and SQS Terraform modules
    
    * Add apply outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+output "queue_arn" {
+  value = module.sqs.queue_arn
+}
+
+output "lambda_function_name" {
+  value = module.lambda.function_name
+}
+
+output "lambda_invoke_arn" {
+  value = module.lambda.invoke_arn
+}
