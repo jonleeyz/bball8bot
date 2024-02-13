7b9ebe0c6271f0c53bedaa36e9766818db3b018d
jonlee <lee.jonathan@hey.com>
2024-02-13 23:38:05 +0800

    Set integration http method argument

--- terraform/api_gateway/main.tf
+++ terraform/api_gateway/main.tf
@@ -34,9 +34,10 @@ resource "aws_api_gateway_integration" "bball8bot" {
   rest_api_id = aws_api_gateway_rest_api.bball8bot.id
   resource_id = aws_api_gateway_resource.bball8bot.id
 
-  http_method = aws_api_gateway_method.bball8bot.http_method
-  type        = "AWS"
-  uri         = "arn:aws:apigateway:ap-southeast-1:sqs:path/${var.queue_name}"
+  http_method             = aws_api_gateway_method.bball8bot.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:ap-southeast-1:sqs:path/${var.queue_name}"
 
   # TODO @jonlee: Understand why this is needed
   request_parameters = {
