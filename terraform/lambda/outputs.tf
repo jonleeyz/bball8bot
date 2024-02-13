d368b33ee795967eb3b33645794aeea9a479bc3a
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-13 10:23:37 +0800

    Update Terraform apply outputs (#32)
    * Add outputs for API Gateway and SQS Terraform modules
    
    * Add apply outputs

---
+++ terraform/lambda/outputs.tf
@@ -0,0 +1,7 @@
+output "function_name" {
+  value = aws_lambda_function.bball8bot_event_handler.function_name
+}
+
+output "invoke_arn" {
+  value = aws_lambda_function.bball8bot_event_handler.invoke_arn
+}
