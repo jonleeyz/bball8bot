80d3a861287c11b585479c290e37a1eb65907751
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-21 13:53:44 +0800

    Create terraform.tfvars file (#41)
    * Create terraform.tfvars file
    
    It's more intuitive to have the values for environments variables set here rather than in a script.
    
    * Update scripts to remove redundant environement variable setting

--- plan.sh
+++ plan.sh
@@ -2,5 +2,4 @@ cd src
 GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o ../terraform/bin/bootstrap main.go
 cd ../terraform
 terraform init
-export TF_VAR_aws_region=ap-southeast-1
 terraform plan
