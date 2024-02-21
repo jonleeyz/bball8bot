78709b123f444dcfe45c2ea9a770f2c248aecac7
jonlee <lee.jonathan@hey.com>
2024-02-21 13:37:42 +0800

    Update variable descriptions for consistency

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "aws_region" {
-  description = "The AWS region that the API Gateway resource will be provisioned in"
+  description = "The AWS region that that resources will be provisioned in, should any of them need to belong to one"
   type        = string
 }
