7b6f91f775329910608eb2579ebaf2123c559f39
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-25 23:27:00 +0800

    Provision Secrets Manager resource (#44)
    * Define input for Secrets Manager resource
    
    * Provision Secrets Manager resource

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,9 @@ variable "aws_region" {
   description = "The AWS region that that resources will be provisioned in, should any of them need to belong to one"
   type        = string
 }
+
+variable "telegram_bot_token" {
+  description = "The Telegram Bot API token for bball8bot"
+  type        = string
+  sensitive   = true
+}
