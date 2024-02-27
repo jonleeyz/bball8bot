d9a3f03c2bb4e1ef334bb913aacac3253daeb245
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-02-27 19:10:54 +0800

    Update terraform apply outputs (#57)
    * Output API Gateway resources values: invoke URL, resource path and resource method
    
    * Remove output: Lambda invoke ARN

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,14 @@ output "lambda_name" {
   value = module.lambda.function_name
 }
 
-output "lambda_invoke_arn" {
-  value = module.lambda.invoke_arn
+output "api_invoke_url" {
+  value = module.api_gateway.invoke_url
+}
+
+output "api_resource_path" {
+  value = module.api_gateway.resource_path
+}
+
+output "api_resource_method" {
+  value = module.api_gateway.resource_method
 }
