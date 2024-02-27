36c1d68478977be7937f8027096e8aff6621f2b7
jonlee <lee.jonathan@hey.com>
2024-02-27 21:55:40 +0800

    Initialise CI Terraform workspace

---
+++ ci/terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+
+  backend "s3" {
+    region = "ap-southeast-1"
+
+    bucket = "jl-terraform-remote-state-store"
+    key    = "bball8bot/ci/terraform.tfstate"
+
+    dynamodb_table = "terraform_state_lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
