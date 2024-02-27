5879521b7e19ea6dc53e7db9ce56cf2c228f7ae1
jonlee <lee.jonathan@hey.com>
2024-02-27 21:53:55 +0800

    Update infra TF workspace S3 state key
    State key needs to be updated to prevent conflict with CI TF workspace S3 state key.

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     region = "ap-southeast-1"
 
     bucket = "jl-terraform-remote-state-store"
-    key    = "bball8bot/terraform.tfstate"
+    key    = "bball8bot/infra/terraform.tfstate"
 
     dynamodb_table = "terraform_state_lock"
   }
