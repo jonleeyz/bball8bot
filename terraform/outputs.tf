4f3639a6e617b10e3184ccf930efddef66b78e35
jonlee <lee.jonathan@hey.com>
2024-02-27 19:06:19 +0800

    Output API Gateway resources values: invoke URL, resource path and resource method

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,18 @@ output "lambda_name" {
   value = module.lambda.function_name
 }
 
+output "api_invoke_url" {
+  value = module.api_gateway.invoke_url
+}
+
+output "api_resource_path" {
+  value = module.api_gateway.resource_path
+}
+
+output "api_resource_method" {
+  value = module.api_gateway.resource_method
+}
+
 output "lambda_invoke_arn" {
   value = module.lambda.invoke_arn
 }
