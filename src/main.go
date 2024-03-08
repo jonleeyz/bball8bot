f1c12097e041681c765c9444dd91f6d12eb7853c
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-03-08 17:22:59 +0800

    Implement basic Lambda echoing behaviour (#78)
    * Create helper function: getBotToken
    
    * Update implementation: handleRequest

--- src/main.go
+++ src/main.go
@@ -2,20 +2,85 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type SecretResponse struct {
+	SecretString string `json:"SecretString"`
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
-	if event == nil {
-		return nil, fmt.Errorf("received nil event")
+// getBotToken requests the Telegram bot token from AWS Secrets Manager and returns it for use.
+// This implementation makes use of the AWS Parameters and Secrets Lambda Extension and only works when a Lambda function
+// executes it.
+// Ref: https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets_lambda.html
+func getBotToken() (string, error) {
+	client := &http.Client{}
+
+	// 1. Prepare HTTP request to get bot token
+	req, err := http.NewRequest("GET", "http://localhost:2773/secretsmanager/get?secretId=telegram_bot_token", nil)
+	if err != nil {
+		return "", err
 	}
-	message := fmt.Sprintf("Hello %s!", event.Name)
-	return &message, nil
+	req.Header.Add("X-Aws-Parameters-Secrets-Token", os.Getenv("AWS_SESSION_TOKEN"))
+
+	// 2. Execute request
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	// 3. Unmarshal for token
+	var secret SecretResponse
+	if err := json.NewDecoder(resp.Body).Decode(&secret); err != nil {
+		return "", err
+	}
+
+	return secret.SecretString, nil
+}
+
+func HandleRequest(ctx context.Context, event *events.SQSEvent) error {
+	if event == nil {
+		errString := "error: event is nil"
+		log.Fatal(errString)
+		return fmt.Errorf(errString)
+	}
+
+	token, err := getBotToken()
+	if err != nil {
+		log.Fatalf("error when retrieving Telegram bot token: %v", err)
+		return err
+	}
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatalf("error when creating Telegram bot: %v", err)
+		return err
+	}
+
+	log.Printf("handler log: %v\n", event.Records)
+
+	for _, record := range event.Records {
+		var update tgbotapi.Update
+		if err := json.Unmarshal([]byte(record.Body), &update); err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if _, err := bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)); err != nil {
+			log.Println(err)
+			continue
+		}
+	}
+	return nil
 }
 
 func main() {
