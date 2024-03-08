c596a2ef74a7e55e35e6828b7c64b316da9b3ad0
jonlee <lee.jonathan@hey.com>
2024-03-09 00:08:57 +0800

    Reorganise logging helper function code chunk

--- src/main.go
+++ src/main.go
@@ -131,13 +131,3 @@ func readDebugLoggingFlag() {
 		isDebugLoggingEnabled = true
 	}
 }
-
-// logInManualDebugMode is a syntatic wrapper around the log.Printf function that creates a log entry with debug tags.
-func logInManualDebugMode(message string, debugObject ...interface{}) {
-	if !isDebugLoggingEnabled {
-		return
-	}
-
-	debugLog := fmt.Sprintf(message, debugObject...)
-	log.Printf("[MANUAL_DEBUG_LOG] %s", debugLog)
-}
