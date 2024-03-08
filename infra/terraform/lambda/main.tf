480e90e5ba046e2f88865967061aaba42762c4da
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-03-08 16:27:01 +0800

    Configure Parameters and Secrets extension layer cache (#76)

--- infra/terraform/lambda/main.tf
+++ infra/terraform/lambda/main.tf
@@ -8,6 +8,13 @@ resource "aws_lambda_function" "bball8bot_event_handler" {
   filename         = local.output_archive_path
   source_code_hash = data.archive_file.zipped_binary_for_deploy.output_base64sha256
   layers           = [local.parameters_and_secrets_extension_layer_arn]
+
+  environment {
+    variables = {
+      PARAMETERS_SECRETS_EXTENSION_CACHE_ENABLED = "true"
+      PARAMETERS_SECRETS_EXTENSION_LOG_LEVEL     = "debug"
+    }
+  }
 }
 
 ##### Enables Lambda event handler to be triggered by SQS events
