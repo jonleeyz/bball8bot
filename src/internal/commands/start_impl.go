302db6f042b59512ef7da8156eb69321074ed27c
jonlee <lee.jonathan@hey.com>
2024-03-12 01:11:23 +0800

    Implement /start handler

---
+++ src/internal/commands/start_impl.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jonleeyz/bbball8bot/internal/logging"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+// Handle initiates the handling flow for the "/start" command.
+func (c *StartCommandHandlerImpl) Handle(ctx context.Context) error {
+	startMessageResponse := tgbotapi.NewMessage(c.update.Message.Chat.ID, START_COMMAND_RESPONSE_TEXT)
+
+	if _, err := c.bot.Send(startMessageResponse); err != nil {
+		err = fmt.Errorf("error when calling Telegram Bot API to send message: %v", err)
+
+		logging.Printf(err.Error())
+		return err
+	}
+	return nil
+}
+
+const START_COMMAND_RESPONSE_TEXT = "Hello from bball8bot, Basketball Ultimate's helper robot!"
