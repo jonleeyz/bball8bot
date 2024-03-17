f17538e2a6b5486f9b4af68f4943cf2c08825ed2
jonlee <lee.jonathan@hey.com>
2024-03-17 21:54:47 +0800

    Update prd infra TF workspace bucket key in CI IAM roles

--- ci/terraform/data.tf
+++ ci/terraform/data.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "terraform_state_management_policy" {
     ]
 
     resources = [
-      "${local.state_store_bucket_arn}/${local.infra_workspace_bucket_key}",
+      "${local.state_store_bucket_arn}/${local.infra_workspace_bucket_key_prd}",
       "${local.state_store_bucket_arn}/${local.ci_workspace_bucket_key}"
     ]
   }
