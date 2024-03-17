02e3e37392fdf17ec35bc29f515adbaf44b42c2d
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2024-03-17 22:12:03 +0800

    Update infra bucket keys in CI IAM policies (#121)
    * Update prd infra TF workspace bucket key in CI IAM roles
    
    * Re-add old infra TF workspace bucket key
    
    Needed to enable prd infra state file migration.

--- ci/terraform/locals.tf
+++ ci/terraform/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  github_oidc_provider_url   = "token.actions.githubusercontent.com"
-  state_store_bucket_arn     = "arn:aws:s3:::jl-terraform-remote-state-store"
-  infra_workspace_bucket_key = "bball8bot/infra/terraform.tfstate"
-  ci_workspace_bucket_key    = "bball8bot/ci/terraform.tfstate"
-  state_lock_table_arn       = "arn:aws:dynamodb:ap-southeast-1:574182556674:table/terraform_state_lock"
+  github_oidc_provider_url       = "token.actions.githubusercontent.com"
+  state_store_bucket_arn         = "arn:aws:s3:::jl-terraform-remote-state-store"
+  infra_workspace_bucket_key_prd = "bball8bot/infra/prd/terraform.tfstate"
+  ci_workspace_bucket_key        = "bball8bot/ci/terraform.tfstate"
+  state_lock_table_arn           = "arn:aws:dynamodb:ap-southeast-1:574182556674:table/terraform_state_lock"
+  infra_workspace_bucket_key_old = "bball8bot/infra/terraform.tfstate"
 }
