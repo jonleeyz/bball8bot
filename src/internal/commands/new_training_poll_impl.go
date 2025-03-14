16f0ffa132585bfeb041f8d583077145bd9bec0f
jonlee <lee.jonathan@hey.com>
2025-03-14 15:50:55 +0800

    Add inline keyboard markup to training poll template

--- src/internal/commands/new_training_poll_impl.go
+++ src/internal/commands/new_training_poll_impl.go
@@ -20,6 +20,7 @@ func (h *NewTrainingPollCommandHandlerImpl) Handle(ctx context.Context) error {
 
 	trainingPollMessageResponse := tgbotapi.NewMessage(h.update.Message.Chat.ID, trainingPollMessageContent)
 	trainingPollMessageResponse.ParseMode = "MarkdownV2"
+	trainingPollMessageResponse.ReplyMarkup = buildInlineKeyboard()
 
 	if _, err := h.bot.Send(trainingPollMessageResponse); err != nil {
 		err = fmt.Errorf("error when calling Telegram Bot API to send message: %v", err)
