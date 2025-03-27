8ad767f33ba4d11db9a37e05f8b6de3ae7477c7c
jonlee <lee.jonathan@hey.com>
2025-03-27 17:41:34 +0800

    Restructure: Create driver file - to encapsulate update handling logic

---
+++ src/driver.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+// handleUpdate parses the input Update object, and responds accordingly.
+func handleUpdate(ctx context.Context, update *tgbotapi.Update, bot *tgbotapi.BotAPI) {
+}
