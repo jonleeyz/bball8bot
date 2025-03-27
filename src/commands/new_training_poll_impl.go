ac3fe9574b74776207a32cea2ddcfcf7809b831b
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2025-03-28 01:04:47 +0800

    Correct go module name typo (#152)

--- src/commands/new_training_poll_impl.go
+++ src/commands/new_training_poll_impl.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jonleeyz/bbball8bot/internal/logging"
+	"github.com/jonleeyz/bball8bot/internal/logging"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
