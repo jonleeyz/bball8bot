fbab782ec485e232351da55794a3118438f1bc3e
jonleeyz <32831680+jonleeyz@users.noreply.github.com>
2025-03-28 11:34:38 +0800

    Update update handler implementations to encapsulate Update information (#155)
    * Update handlers to encapsulate update information
    
    Handlers are selected and initialised based on the specific Update's data; it makes sense that they and their functionality should be coupled to the data."
    
    * Update UpdateHandler interface and implementations to not require an input Update object

--- src/internal/handlers/callbacks/main_handler.go
+++ src/internal/handlers/callbacks/main_handler.go
@@ -10,19 +10,20 @@ import (
 )
 
 type CallbackQueryHandler struct {
-	bot *tgbotapi.BotAPI
+	bot    *tgbotapi.BotAPI
+	update *tgbotapi.Update
 }
 
-func Init(bot *tgbotapi.BotAPI) (*CallbackQueryHandler, error) {
+func Init(bot *tgbotapi.BotAPI, update *tgbotapi.Update) (*CallbackQueryHandler, error) {
 	if bot == nil {
 		return nil, fmt.Errorf("error when creating callback query handler: %s", customerrors.ERROR_MESSAGE_NIL_INPUT_BOT)
 	}
 
-	return &CallbackQueryHandler{bot: bot}, nil
+	return &CallbackQueryHandler{bot: bot, update: update}, nil
 }
 
-func (h *CallbackQueryHandler) Handle(ctx context.Context, update *tgbotapi.Update) error {
-	callback := update.CallbackQuery
+func (h *CallbackQueryHandler) Handle(ctx context.Context) error {
+	callback := h.update.CallbackQuery
 	callbackResponseString := fmt.Sprintf("button pressed: %s", callback.Data)
 
 	var callbackAnswer tgbotapi.CallbackConfig
