9d963abf32f910a8f2ca24f35ab6dd0ab8bc5823
jonlee <lee.jonathan@hey.com>
2025-03-28 18:25:59 +0800

    Define callback data string constants

---
+++ src/internal/handlers/callbacks/consts.go
@@ -0,0 +1,6 @@
+package callbacks
+
+// callback data
+const CALLBACK_DATA_ATTENDING = "callback_ATTENDING"
+const CALLBACK_DATA_NOT_ATTENDING = "callback_NOT_ATTENDING"
+const CALLBACK_DATA_CONFIRMING = "callback_CONFIRMING"
