1ae4447178801082a443c4814529bed5bd37742a
jonlee <lee.jonathan@hey.com>
2025-03-28 10:46:59 +0800

    Update HandleUpdate implementation to return an error value if needed

--- src/internal/handlers/main.go
+++ src/internal/handlers/main.go
@@ -4,19 +4,14 @@ import (
 	"context"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/jonleeyz/bball8bot/internal/logging"
 )
 
-// HandleUpdate parses the input Update object, and responds accordingly.
-// No error is returned from this function, any error is logged but is not fatal; other updates can still be processed.
-// TODO @jonlee: Change signature to return error; not returning because of Telegram's requirement does not allow the function to be general purpose
-func HandleUpdate(ctx context.Context, update *tgbotapi.Update, bot *tgbotapi.BotAPI) {
+func HandleUpdate(ctx context.Context, update *tgbotapi.Update, bot *tgbotapi.BotAPI) error {
 	h, err := getUpdateHandler(ctx, bot, update)
 	if err != nil {
-		logging.Errorf("%s", err.Error())
-		return
+		return err
 	}
-	h.Handle(ctx, update)
+	return h.Handle(ctx, update)
 }
 
 type UpdateHandler interface {
