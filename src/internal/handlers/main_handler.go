c150d7428af1c9cd4b6a67f7a81a53fed6f57a6c
jonlee <lee.jonathan@hey.com>
2025-03-28 09:44:17 +0800

    Refactor: Rename getHandler -> getUpdateHandler

--- src/internal/handlers/main_handler.go
+++ src/internal/handlers/main_handler.go
@@ -14,7 +14,7 @@ import (
 // No error is returned from this function, any error is logged but is not fatal; other updates can still be processed.
 // TODO @jonlee: Change signature to return error; not returning because of Telegram's requirement does not allow the function to be general purpose
 func HandleUpdate(ctx context.Context, update *tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	h, err := getHandler(ctx, bot, update)
+	h, err := getUpdateHandler(ctx, bot, update)
 	if err != nil {
 		return
 	}
@@ -23,8 +23,8 @@ func HandleUpdate(ctx context.Context, update *tgbotapi.Update, bot *tgbotapi.Bo
 
 // Pre-condition: Exactly 1 optional field in an Update object will be non-nil.
 // Reference: https://core.telegram.org/bots/api#update
-// getHandler returns a new handler appropriate to handle the input Update's content.
-func getHandler(ctx context.Context, bot *tgbotapi.BotAPI, update *tgbotapi.Update) (UpdateHandler, error) {
+// getUpdateHandler returns a new handler appropriate to handle the input Update's content.
+func getUpdateHandler(ctx context.Context, bot *tgbotapi.BotAPI, update *tgbotapi.Update) (UpdateHandler, error) {
 	if bot == nil {
 		return nil, errors.New("bot provided is nil")
 	}
